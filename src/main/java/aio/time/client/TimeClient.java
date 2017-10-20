e0d02209c32e025ab8aa2cd8adc32226fd200d29
schuka <709443543@qq.com>
2017-10-20 21:04:41 +0800

    修改主干

--- src/main/java/aio/time/client/TimeClient.java
+++ src/main/java/aio/time/client/TimeClient.java
@@ -4,5 +4,7 @@ public class TimeClient {
 	public static void main(String[] args){
 		int port = 8080;
 		new Thread(new AsyncTimeClientHandler("127.0.0.1", port), "AIO-AsyncTimeClientHandler-001").start();
+		System.out.println(1);
+		System.out.println(2);
 	}
 }
