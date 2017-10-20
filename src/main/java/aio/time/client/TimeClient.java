fe5cc55080e25be2bad73cbe31d9bac3e1f61ccd
schuka <709443543@qq.com>
2017-10-20 21:23:55 +0800

    11

--- src/main/java/aio/time/client/TimeClient.java
+++ src/main/java/aio/time/client/TimeClient.java
@@ -8,6 +8,7 @@ public class TimeClient {
 		System.out.println(2);
 		///1234132413433
 		System.out.println("local update");
+		System.out.println("local update");
 		System.out.println("github update");
 	}
 }
